1d976aa7c037fe34be03bddcc182eee777f35077
huali2580 <96482741+huali2580@users.noreply.github.com>
2021-12-21 23:17:29 +0800

    Update 商品打九折.py
    修改格式

--- 商品打九折.py
+++ 商品打九折.py
@@ -1,8 +1,7 @@
-print('結賬系統')
-cost = int(input('請輸入商品價格:'))
-if cost >= 2000:
-    print('您的商品已打折。')
-    print(cost*0.9)
+print('##結賬系統##')
+cost = int(input('輸入購買價格=>'))
+if cost >=2000:
+	print('價格結算:',cost*0.9)
 else :
-    print(cost)
+	print('價格結算:',cost)
 print('謝謝惠顧')
